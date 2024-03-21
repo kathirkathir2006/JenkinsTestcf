105511bad66ebc235b983f984797c1ee23ceb2ca
kathirkathir2006 <kathirkathir2006@gmail.com>
2024-03-21 16:47:58 +0530

    Update App.java

--- src/main/java/com/clayfin/java/training/mavendemo/App.java
+++ src/main/java/com/clayfin/java/training/mavendemo/App.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( Strin[] args )
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
-         System.out.println( "Hello World!" );
+         Syste.out.println( "Hello World!" );
             
     }
 }
